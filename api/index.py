515c3a3dcf7ea4f5af6a7132e34465eda75b93a5
marketcalls <rajandran@marketcalls.in>
2024-02-22 11:37:03 +0530

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -308,11 +308,11 @@ def place_order():
                 }), 500
         else:
             # If 'data' is null or status is not 200, extract the message and return as error
-            message = response_data['data'].get('message', 'Failed to place order')
+            message = response_data.get('message', 'Failed to place order')
             return jsonify({
                 'status': 'error',
                 'message': message,
-                'details': response_data
+                
             }), res.status if res.status != 200 else 500  # Use the API's status code, unless it's 200 but 'data' is null
     
     except KeyError as e:
