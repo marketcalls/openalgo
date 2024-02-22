455edfff9e98024a08058d91e454325f0ab66c5a
marketcalls <rajandran@marketcalls.in>
2024-02-22 16:32:52 +0530

    modified:   api/cron.py modified:   vercel.json

--- api/cron.py
+++ api/cron.py
@@ -1,2 +1,9 @@
+from flask import Flask, jsonify
 
-print("Cron Job Ran Successfully")
+app = Flask(__name__)
+
+@app.route('/api/cron', methods=['GET'])
+def cron_job():
+    # This is where you would put the logic you want to run on a schedule
+    print('Cron job ran successfully')
+    return jsonify(message='Cron job ran successfully'), 200
