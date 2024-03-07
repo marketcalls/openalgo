c3d01dac1ea07cc2832ab67fb8888871c87ecee2
marketcalls <rajandran@marketcalls.in>
2024-03-07 17:12:31 +0530

    new file:   static/css/main.css 	new file:   static/js/mobile-menu.js 	modified:   templates/base.html

---
+++ static/js/mobile-menu.js
@@ -0,0 +1,12 @@
+// Functions for mobile menu toggle (existing functionality)
+function toggleMobileMenu() {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.remove('-translate-x-full');
+    document.querySelector('button[onclick="toggleMobileMenu()"]').style.display = 'none';
+}
+
+function closeMobileMenu() {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.add('-translate-x-full');
+    document.querySelector('button[onclick="toggleMobileMenu()"]').style.display = 'block';
+}
