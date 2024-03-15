ec9209a3bfdf46f57efa57d0fb54e3e3fb633a9c
marketcalls <rajandran@marketcalls.in>
2024-03-15 16:33:38 +0530

    modified:   blueprints/tv_json.py 	modified:   database/tv_search.py 	modified:   templates/tradingview.html

--- database/tv_search.py
+++ database/tv_search.py
@@ -3,6 +3,5 @@
 from database.master_contract_db import SymToken
 #from database.db import db_session
 
-def search_symbols(symbol):
-    # The "%" is a wildcard for the LIKE SQL operator
-    return SymToken.query.filter(SymToken.symbol == symbol).all()
+def search_symbols(symbol,exchange):
+    return SymToken.query.filter(SymToken.symbol == symbol,SymToken.exch_seg == exchange).all()
