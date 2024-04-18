2f625d1c98ac277be6e568ee3f98fd9566e0028a
marketcalls <rajandran@marketcalls.in>
2024-04-18 16:38:40 +0530

    modified:   blueprints/brlogin.py 	new file:   broker/dhan/__init__.py 	new file:   broker/dhan/api/__init__.py 	new file:   broker/dhan/api/auth_api.py 	new file:   broker/dhan/api/funds.py 	new file:   broker/dhan/api/order_api.py 	new file:   broker/dhan/database/master_contract_db.py 	new file:   broker/dhan/mapping/order_data.py 	new file:   broker/dhan/mapping/transform_data.py 	modified:   templates/broker.html

---
+++ broker/dhan/mapping/transform_data.py
@@ -0,0 +1,136 @@
+#Mapping OpenAlgo API Request https://openalgo.in/docs
+#Mapping Upstox Broking Parameters https://dhanhq.co/docs/v1/orders/
+
+def transform_data(data,token):
+    """
+    Transforms the new API request structure to the current expected structure.
+    """
+    drv_expiry_date = None
+    drv_options_type = None
+    drv_strike_price = None
+
+
+
+    # Basic mapping
+    transformed = {
+        "dhanClientId": data["apikey"],
+        "correlationId":None,
+        "transactionType": data['action'].upper(),
+        "exchangeSegment": map_exchange_type(data["exchange"]),
+        "productType": map_product_type(data["product"]),
+        "orderType": map_order_type(data["pricetype"]),
+        "validity":"DAY",
+        "securityId": token,
+        "quantity": int(data["quantity"]),
+        "disclosedQuantity": int(data.get("disclosed_quantity", 0)),
+
+        "price": float(data.get("price", 0)),
+        "afterMarketOrder": False,
+        "boProfitValue": None,
+        "boStopLossValue": None,
+        "drvExpiryDate": drv_expiry_date,  
+        "drvOptionType": drv_options_type,
+        "drvStrikePrice": drv_strike_price  # Assuming false as default; you might need logic to handle this if it can vary
+    }
+
+
+    # Extended mapping for fields that might need conditional logic or additional processing
+    transformed["disclosed_quantity"] = data.get("disclosed_quantity", "0")
+    transformed["trigger_price"] = data.get("trigger_price", "0")
+    
+    return transformed
+
+
+def transform_modify_order_data(data):
+    return {
+        "quantity": data["quantity"],
+        "validity": "DAY",
+        "price": data["price"],
+        "order_id": data["orderid"],
+        "order_type": map_order_type(data["pricetype"]),
+        "disclosed_quantity": data.get("disclosed_quantity", "0"),
+        "trigger_price": data.get("trigger_price", "0")
+    }
+
+
+
+def map_order_type(pricetype):
+    """
+    Maps the new pricetype to the existing order type.
+    """
+    order_type_mapping = {
+        "MARKET": "MARKET",
+        "LIMIT": "LIMIT",
+        "SL": "STOP_LOSS",
+        "SL-M": "STOP_LOSS_MARKET"
+    }
+    return order_type_mapping.get(pricetype, "MARKET")  # Default to MARKET if not found
+
+
+
+def map_exchange_type(exchange):
+    """
+    Maps the Broker Exchange to the OpenAlgo Exchange.
+    """
+    exchange_mapping = {
+        "NSE": "NSE_EQ",
+        "BSE": "BSE_EQ",
+        "CDS": "NSE_CURRENCY",
+        "NFO": "NSE_FNO",
+        "BFO": "BSE_FNO",
+        "BCD": "BSE_CURRENCY",
+        "MCX": "MCX_COMM"
+
+    }
+    return exchange_mapping.get(exchange)  # Default to MARKET if not found
+
+
+
+def map_exchange(brexchange):
+    """
+    Maps the Broker Exchange to the OpenAlgo Exchange.
+    """
+    exchange_mapping = {
+        "NSE_EQ": "NSE",
+        "BSE_EQ": "BSE",
+        "NSE_CURRENCY": "CDS",
+        "NSE_FNO": "NFO",
+        "BSE_FNO": "BFO",
+        "BSE_CURRENCY": "BCD",
+        "MCX_COMM": "MCX"
+
+    }
+    return exchange_mapping.get(brexchange)  # Default to MARKET if not found
+
+
+
+def map_product_type(product):
+    """
+    Maps the new product type to the existing product type.
+    """
+    product_type_mapping = {
+        "CNC": "CNC",
+        "NRML": "MARGIN",
+        "MIS": "INTRADAY",
+    }
+    return product_type_mapping.get(product, "INTRADAY")  # Default to INTRADAY if not found
+
+def reverse_map_product_type(exchange,product):
+    """
+    Reverse maps the broker product type to the OpenAlgo product type, considering the exchange.
+    """
+    # Exchange to OpenAlgo product type mapping for 'D'
+    exchange_mapping_for_d = {
+        "NSE": "CNC",
+        "BSE": "CNC",
+        "NFO": "NRML",
+        "BFO": "NRML",
+        "MCX": "NRML",
+        "CDS": "NRML",
+    }
+    
+    # Reverse mapping based on product type and exchange
+    if product == 'D':
+        return exchange_mapping_for_d.get(exchange)  # Removed default; will return None if not found
+    elif product == 'I':
+        return "MIS"
