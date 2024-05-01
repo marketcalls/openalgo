28bf24f280bcc67328e78b1794984597ad5344c5
Siva Rama Krishna Reddy Kristipati <siva.kristipati@beghouconsulting.com>
2024-05-01 18:21:32 +0530

    Updated code to place orders.

--- broker/kotak/mapping/transform_data.py
+++ broker/kotak/mapping/transform_data.py
@@ -10,27 +10,22 @@ def transform_data(data,token):
     symbol = get_br_symbol(data["symbol"],data["exchange"])
     # Basic mapping
     transformed = {
-        "apikey": data["apikey"],
-        "variety": map_variety(data["pricetype"]),
-        "tradingsymbol": symbol,
-        "symboltoken": token,
-        "transactiontype": data["action"].upper(),
-        "exchange": data["exchange"],
-        "ordertype": map_order_type(data["pricetype"]),
-        "producttype": map_product_type(data["product"]),
-        "duration": "DAY",  # Assuming DAY as default; you might need logic to handle this if it can vary
-        "price": data.get("price", "0"),
-        "squareoff": "0",  # Assuming not applicable; adjust if needed
-        "stoploss": data.get("trigger_price", "0"),  
-        "disclosedquantity": data.get("disclosed_quantity", "0"),
-        "quantity": data["quantity"]
+        "am":"NO",
+        "dq":data.get("disclosed_quantity", "0"),
+        "bc":"1",
+        "es":reverse_map_exchange(data["exchange"]),
+        "mp":"0",
+        "pc":data.get("product", "MIS"),
+        "pf":"N",
+        "pr":data.get("price", "0"),
+        "pt":map_order_type(data["pricetype"]),
+        "qt":data["quantity"],
+        "rt":"DAY",
+        "tp":data.get("trigger_price", "0"),
+        "ts":symbol,
+        "tt":'B' if data['action'] == 'BUY' else ('S' if data['action'] == 'SELL' else 'None'),
+        "ig":"OpenAlgo"
     }
-
-
-    # Extended mapping for fields that might need conditional logic or additional processing
-    transformed["disclosedquantity"] = data.get("disclosed_quantity", "0")
-    transformed["triggerprice"] = data.get("trigger_price", "0")
-    
     return transformed
 
 
@@ -57,10 +52,10 @@ def map_order_type(pricetype):
     Maps the new pricetype to the existing order type.
     """
     order_type_mapping = {
-        "MARKET": "MARKET",
-        "LIMIT": "LIMIT",
-        "SL": "STOPLOSS_LIMIT",
-        "SL-M": "STOPLOSS_MARKET"
+        "MARKET": "MKT",
+        "LIMIT": "L",
+        "SL": "SL",
+        "SL-M": "SL-M"
     }
     return order_type_mapping.get(pricetype, "MARKET")  # Default to MARKET if not found
 
@@ -88,6 +83,39 @@ def map_variety(pricetype):
     }
     return variety_mapping.get(pricetype, "NORMAL")  # Default to DELIVERY if not found
 
+def map_exchange(brexchange):
+    """
+    Maps the Broker Exchange to the OpenAlgo Exchange.
+    """
+    
+
+    exchange_mapping = {
+        "nse_cm": "NSE",
+        "bse_cm": "BSE",
+        "cde_fo": "CDS",
+        "nse_fo": "NFO",
+        "bse_fo": "BFO",
+        "bcs_fo": "BCD",
+        "mcx_fo": "MCX"
+
+    }
+    return exchange_mapping.get(brexchange)
+
+def reverse_map_exchange(exchange):
+    """
+    Maps the Broker Exchange to the OpenAlgo Exchange.
+    """
+
+    exchange_mapping = {
+        "NSE": "nse_cm",
+        "BSE": "bse_cm",
+        "CDS": "cde_fo",
+        "NFO": "nse_fo",
+        "BFO": "bse_fo",
+        "BCD": "bcs_fo",
+        "MCX": "mcx_fo"
+    }
+    return exchange_mapping.get(exchange)
 
 def reverse_map_product_type(product):
     """
