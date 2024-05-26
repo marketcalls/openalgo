dcc4679aac6a6a75d17e58c58b5ccddbae707caa
marketcalls <rajandran@marketcalls.in>
2024-05-27 00:15:51 +0530

    Footer Template Fix and Kotak Base Preparation with PyJWT

--- utils/config.py
+++ utils/config.py
@@ -9,6 +9,9 @@ load_dotenv()
 def get_broker_api_key():
     return os.getenv('BROKER_API_KEY')
 
+def get_broker_api_secret():
+    return os.getenv('BROKER_API_SECRET')
+
 def get_login_rate_limit_min():
     return os.getenv("LOGIN_RATE_LIMIT_MIN", "5 per minute")
 
