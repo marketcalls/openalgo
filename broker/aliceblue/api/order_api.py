5d4815c1a823ce42384e0d0cdbb564c57b778722
Deepanshu Goyal <deepu160404@gmail.com>
2024-07-16 17:30:01 +0530

    Final_commit

--- broker/aliceblue/api/order_api.py
+++ broker/aliceblue/api/order_api.py
@@ -24,6 +24,7 @@ def get_api_response(endpoint, auth, method="GET", payload=''):
     return json.loads(data.decode("utf-8"))
 
 def get_order_book(auth):
+
     return get_api_response("/rest/AliceBlueAPIService/api/placeOrder/fetchOrderBook",auth)
 
 def get_trade_book(auth):
@@ -250,70 +251,67 @@ def close_all_positions(current_api_key,auth):
 def cancel_order(orderid,auth):
     # Assuming you have a function to get the authentication token
     AUTH_TOKEN = auth
-    
+    order_book_response = get_order_book(AUTH_TOKEN)
     # Set up the request headers
     headers = {
-        'X-Kite-Version': '3',
-        'Authorization': f'token {AUTH_TOKEN}',
-    }
-    
+    'Authorization': f'Bearer {get_broker_api_key()} {AUTH_TOKEN}',
+    'Content-Type': 'application/json'
+    }      
+    Trading_symbol = ""
+    Exchange = ""
+    orders = (order_book_response)
+    for order in orders:
+        if order.get("Nstordno") == orderid:
+            Trading_symbol = order.get("Trsym")
+            Exchange = order.get("Exchange")
+
     # Prepare the payload
-    payload = ''
+    payload = json.dumps({
+        "exch": Exchange,
+        "nestOrderNumber": orderid,
+        "trading_symbol": Trading_symbol
+    })
     
     # Establish the connection and send the request
-    conn = http.client.HTTPSConnection("api.kite.trade")  # Adjust the URL as necessary
-    conn.request("DELETE", f"/orders/regular/{orderid}", payload, headers)
+    conn = http.client.HTTPSConnection("ant.aliceblueonline.com")
+    conn.request("POST", "/rest/AliceBlueAPIService/api/placeOrder/cancelOrder", payload, headers)
     res = conn.getresponse()
-    data = json.loads(res.read().decode("utf-8"))
-    print(data)
-    
+    response_data = json.loads(res.read().decode("utf-8"))
+    print(response_data)
+
     # Check if the request was successful
-    if data.get("status"):
+    if response_data.get("stat") == "Ok":
         # Return a success response
-        return {"status": "success", "orderid": data['data']['order_id']}, 200
+        return {"status": "success", "orderid": response_data["nestOrderNumber"]}, 200
     else:
         # Return an error response
-        return {"status": "error", "message": data.get("message", "Failed to cancel order")}, res.status
+        return {"status": "error", "message": response_data.get("emsg", "Failed to cancel order")}, res.status
 
 
 def modify_order(data,auth):
 
-    
-
     AUTH_TOKEN = auth
-    
     newdata = transform_modify_order_data(data)  # You need to implement this function
     
-  
     # Set up the request headers
     headers = {
-        'X-Kite-Version': '3',
-        'Authorization': f'token {AUTH_TOKEN}',
-        'Content-Type': 'application/x-www-form-urlencoded' 
-    }
-    payload = {
-        'order_type': newdata['order_type'],
-        'quantity': newdata['quantity'],
-        'price': newdata['price'],
-        'trigger_price': newdata['trigger_price'],
-        'disclosed_quantity': newdata['disclosed_quantity'],
-        'validity': newdata['validity']
-      }
+    'Authorization': f'Bearer {get_broker_api_key()} {AUTH_TOKEN}',
+    'Content-Type': 'application/json'
+    }    
+    payload = json.dumps(newdata)
 
     print(payload)
 
-    payload =  urllib.parse.urlencode(payload)
-
-    conn = http.client.HTTPSConnection("api.kite.trade")
-    conn.request("PUT", f"/orders/regular/{data['orderid']}", payload, headers)
+    conn = http.client.HTTPSConnection("ant.aliceblueonline.com")
+    conn.request("POST", "/rest/AliceBlueAPIService/api/placeOrder/modifyOrder", payload, headers)
     res = conn.getresponse()
-    data = json.loads(res.read().decode("utf-8"))
-    print(data)
+    response_data = json.loads(res.read().decode("utf-8"))
+    print(response_data)
 
-    if data.get("status") == "success" or data.get("message") == "SUCCESS":
-        return {"status": "success", "orderid": data["data"]["order_id"]}, 200
+    if response_data.get("stat") == "Ok":
+        return {"status": "success", "orderid": response_data["nestOrderNumber"]}, 200
     else:
-        return {"status": "error", "message": data.get("message", "Failed to modify order")}, res.status
+        return {"status": "error", "message": response_data.get("emsg", "Failed to modify order")}, res.status
     
 
 def cancel_all_orders_api(data,auth):
@@ -322,19 +320,20 @@ def cancel_all_orders_api(data,auth):
     # Get the order book
     order_book_response = get_order_book(AUTH_TOKEN)
     #print(order_book_response)
-    if order_book_response['status'] != 'success':
-        return [], []  # Return empty lists indicating failure to retrieve the order book
+    if isinstance(order_book_response, dict):
+        if order_book_response['stat'] == 'Not_Ok':
+            return [], []  # Return empty lists indicating failure to retrieve the order book
 
     # Filter orders that are in 'open' or 'trigger_pending' state
-    orders_to_cancel = [order for order in order_book_response.get('data', [])
-                        if order['status'] in ['OPEN', 'TRIGGER PENDING']]
+    orders_to_cancel = [order for order in order_book_response
+                        if order['Status'] in ['open', 'trigger pending']]
     print(orders_to_cancel)
     canceled_orders = []
     failed_cancellations = []
 
     # Cancel the filtered orders
     for order in orders_to_cancel:
-        orderid = order['order_id']
+        orderid = order['Nstordno']
         cancel_response, status_code = cancel_order(orderid,AUTH_TOKEN)
         if status_code == 200:
             canceled_orders.append(orderid)
--- broker/aliceblue/mapping/transform_data.py
+++ broker/aliceblue/mapping/transform_data.py
@@ -33,14 +33,18 @@ def transform_data(data):
 
 def transform_modify_order_data(data):
     return {
-        "order_type": map_order_type(data["pricetype"]),
-        "quantity": data["quantity"],
-        "price": data["price"],
-        "trigger_price": data.get("trigger_price", "0"),
-        "disclosed_quantity": data.get("disclosed_quantity", "0"),
-        "validity": "DAY"      
+        "discqty": int(data.get("disclosed_quantity", 0)),
+        "exch": data.get("exchange"),
+        "filledQuantity": 0,
+        "nestOrderNumber": data.get("orderid"),
+        "prctyp": map_order_type(data.get("pricetype")),
+        "price": float(data.get("price")),
+        "qty": int(data.get("quantity")),
+        "trading_symbol": get_br_symbol(data.get("symbol"),data.get("exchange")),
+        "trigPrice": data.get("trigger_price", "0"),
+        "transtype": data.get("action").upper(),
+        "pCode": data.get("product")
     }
-
 
 
 def map_order_type(pricetype):
@@ -66,15 +70,16 @@ def map_product_type(product):
     }
     return product_type_mapping.get(product, "MIS")  # Default to INTRADAY if not found
 
-def reverse_map_product_type(exchange,product):
+def reverse_map_product_type(product):
     """
     Reverse maps the broker product type to the OpenAlgo product type, considering the exchange.
     """
     # Exchange to OpenAlgo product type mapping for 'D'
     exchange_mapping = {
-        "CNC": "CNC",
-        "NRML": "NRML",
-        "MIS": "MIS",
+        "MKT": "MARKET",
+        "L": "LIMIT",
+        "SL": "SL",
+        "SL-M": "SL-M"
     }
    
     return exchange_mapping.get(product)
