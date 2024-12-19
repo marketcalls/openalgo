1c769b35e57a1050af15e0b743969f0e3393f07b
marketcalls <rajandran@marketcalls.in>
2024-12-20 00:34:19 +0530

    updating socketurl

--- static/js/socket-events.js
+++ static/js/socket-events.js
@@ -255,7 +255,8 @@ window.refreshCurrentPageContent = function() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const socketUrl = `${location.protocol}//${window.location.hostname}`;
+    const socketUrl = `${location.protocol}//${window.location.hostname}${APPLICATION_ROOT}socket.io`; // Append APPLICATION_ROOT to the socket URL
+
     var socket = io.connect(socketUrl, {
         path: `${APPLICATION_ROOT}/socket.io`,
         transports: ['websocket', 'polling']
