ffc4bf586081578a76b6d0bdf57419fe007bc351
crypt0inf0 <84570125+crypt0inf0@users.noreply.github.com>
2024-12-25 09:08:57 +0000

    Fixing ZeroDivisionError: float division by zero

--- broker/kotak/mapping/order_data.py
+++ broker/kotak/mapping/order_data.py
@@ -242,9 +242,16 @@ def transform_holdings_data(holdings_data):
             "exchange": holding.get('exchangeSegment', ''),
             "quantity": holding.get('quantity', 0),
             "product": holding.get('instrumentType', ''),
-            "pnl": round((float(holding.get('mktValue', 0.0)) - float(holding.get('holdingCost', 0.0))),2),
-            "pnlpercent": round((float(holding.get('mktValue', 0.0)) - float(holding.get('holdingCost', 0.0)))/float(holding.get('holdingCost', 0.0))*100,2)
+            "pnl": round((float(holding.get('mktValue', 0.0)) - float(holding.get('holdingCost', 0.0))), 2),
+            "pnlpercent": round(
+                (
+                    (float(holding.get('mktValue', 0.0)) - float(holding.get('holdingCost', 0.0))) 
+                    / float(holding.get('holdingCost', 0.0)) 
+                    * 100 
+                ) if float(holding.get('holdingCost', 0.0)) != 0 else 0, 2
+            )
         }
+
         transformed_data.append(transformed_position)
     print("Holdings Data")
     print(transformed_data)
