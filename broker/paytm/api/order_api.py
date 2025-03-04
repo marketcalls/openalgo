6222f92adcc0eb20e1eb5ea3ab4f7a0e56a2306f
Naidu Annepu <naiduasn@users.noreply.github.com>
2025-03-04 14:41:47 +0530

    implemnted cancel and cancellAll orders

--- broker/paytm/api/order_api.py
+++ broker/paytm/api/order_api.py
@@ -4,12 +4,11 @@ import os
 import urllib.parse
 from database.auth_db import get_auth_token
 from database.token_db import get_br_symbol, get_oa_symbol
-from broker.paytm.mapping.transform_data import transform_data , map_product_type, reverse_map_product_type, transform_modify_order_data
-
+from broker.paytm.mapping.transform_data import transform_data, map_product_type, reverse_map_product_type, transform_modify_order_data
 
 
 def get_api_response(endpoint, auth, method="GET", payload=''):
-    
+
     AUTH_TOKEN = auth
     conn = http.client.HTTPSConnection("developer.paytmmoney.com")
     headers = {
@@ -23,28 +22,33 @@ def get_api_response(endpoint, auth, method="GET", payload=''):
     data = res.read()
     return json.loads(data.decode("utf-8"))
 
-def get_order_book(auth):
-    return get_api_response("/orders/v1/user/orders",auth)
 
-#PAYTM does not provide all tradebook details. every tradebook call needs orderID
+def get_order_book(auth):
+    return get_api_response("/orders/v1/user/orders", auth)
+
+# PAYTM does not provide all tradebook details. every tradebook call needs orderID
+
+
 def get_trade_book(auth):
-    return get_api_response("/orders/v1/user/orders",auth)
+    return get_api_response("/orders/v1/user/orders", auth)
+
 
 def get_positions(auth):
-    return get_api_response("/orders/v1/position",auth)
+    return get_api_response("/orders/v1/position", auth)
+
 
 def get_holdings(auth):
-    return get_api_response("/holdings/v1/get-user-holdings-data",auth)
+    return get_api_response("/holdings/v1/get-user-holdings-data", auth)
 
-def get_open_position(tradingsymbol, exchange, product,auth):
 
-    #Convert Trading Symbol from OpenAlgo Format to Broker Format Before Search in OpenPosition
-    tradingsymbol = get_br_symbol(tradingsymbol,exchange)
-    
+def get_open_position(tradingsymbol, exchange, product, auth):
+
+    # Convert Trading Symbol from OpenAlgo Format to Broker Format Before Search in OpenPosition
+    tradingsymbol = get_br_symbol(tradingsymbol, exchange)
 
     positions_data = get_positions(auth)
     net_qty = '0'
-    #print(positions_data['data']['net'])
+    # print(positions_data['data']['net'])
 
     if positions_data and positions_data.get('status') and positions_data.get('data'):
         for position in positions_data['data']['net']:
@@ -55,8 +59,9 @@ def get_open_position(tradingsymbol, exchange, product,auth):
 
     return net_qty
 
-def place_order_api(data,auth):
-    
+
+def place_order_api(data, auth):
+
     AUTH_TOKEN = auth
     conn = http.client.HTTPSConnection("developer.paytmmoney.com")
     headers = {
@@ -64,18 +69,17 @@ def place_order_api(data,auth):
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     }
-    
-    payload = transform_data(data)  
+
+    payload = transform_data(data)
 
     payload = json.dumps(payload)
 
-    #payload =  urllib.parse.urlencode(payload)
+    # payload =  urllib.parse.urlencode(payload)
     print(payload)
 
     conn.request("POST", "/orders/v1/place/regular", payload, headers)
     res = conn.getresponse()
     response_data = json.loads(res.read().decode("utf-8"))
-
 
     print(response_data)
 
@@ -86,13 +90,13 @@ def place_order_api(data,auth):
     return res, response_data, orderid
 
 
-def close_all_positions(current_api_key,auth):
+def close_all_positions(current_api_key, auth):
 
     AUTH_TOKEN = auth
     # Fetch the current open positions
     positions_response = get_positions(AUTH_TOKEN)
 
-    #print(positions_response)
+    # print(positions_response)
     # Check if the positions data is null or empty
     if positions_response['data'] is None or not positions_response['data']:
         return {"message": "No Open Positions Found"}, 200
@@ -108,8 +112,9 @@ def close_all_positions(current_api_key,auth):
             action = 'SELL' if int(position['quantity']) > 0 else 'BUY'
             quantity = abs(int(position['quantity']))
 
-            #Get OA Symbol before sending to Place Order
-            symbol = get_oa_symbol(position['tradingsymbol'],position['exchange'])
+            # Get OA Symbol before sending to Place Order
+            symbol = get_oa_symbol(
+                position['tradingsymbol'], position['exchange'])
             # Prepare the order payload
             place_order_payload = {
                 "apikey": current_api_key,
@@ -118,65 +123,76 @@ def close_all_positions(current_api_key,auth):
                 "action": action,
                 "exchange": position['exchange'],
                 "pricetype": "MARKET",
-                "product": reverse_map_product_type(position['exchange'],position['product']),
+                "product": reverse_map_product_type(position['exchange'], position['product']),
                 "quantity": str(quantity)
             }
 
             print(place_order_payload)
 
             # Place the order to close the position
-            _, api_response, _ =   place_order_api(place_order_payload,AUTH_TOKEN)
+            _, api_response, _ = place_order_api(
+                place_order_payload, AUTH_TOKEN)
 
             print(api_response)
-            
+
             # Note: Ensure place_order_api handles any errors and logs accordingly
 
     return {'status': 'success', "message": "All Open Positions SquaredOff"}, 200
 
 
-def cancel_order(orderid,auth):
-    # Assuming you have a function to get the authentication token
+def cancel_order(orderid, auth):
+
     AUTH_TOKEN = auth
-    
-    # Set up the request headers
+    conn = http.client.HTTPSConnection("developer.paytmmoney.com")
     headers = {
-        'X-Kite-Version': '3',
-        'Authorization': f'token {AUTH_TOKEN}',
+        'x-jwt-token': AUTH_TOKEN,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
     }
-    
-    # Prepare the payload
-    payload = ''
-    
-    # Establish the connection and send the request
-    conn = http.client.HTTPSConnection("api.kite.trade")  # Adjust the URL as necessary
-    conn.request("DELETE", f"/orders/regular/{orderid}", payload, headers)
-    res = conn.getresponse()
-    data = json.loads(res.read().decode("utf-8"))
-    print(data)
-    
-    # Check if the request was successful
-    if data.get("status"):
-        # Return a success response
-        return {"status": "success", "orderid": data['data']['order_id']}, 200
-    else:
-        # Return an error response
-        return {"status": "error", "message": data.get("message", "Failed to cancel order")}, res.status
+    orders_list = get_order_book(AUTH_TOKEN)
+    for order in orders_list['data']:
+        if order['order_no'] == orderid:
+            if order['status'] == 'Pending':
+                print("Cancelling order:", orderid)
+                payload = json.dumps({"order_no": orderid, "source": "N",
+                                      "txn_type": order['txn_type'],
+                                      "exchange": order['exchange'],
+                                      "segment": order['segment'],
+                                      "product": order['product'],
+                                      "security_id": order['security_id'],
+                                      "quantity": order['quantity'],
+                                      "validity": order['validity'],
+                                      "order_type": order['order_type'],
+                                      "price": order['price'],
+                                      "off_mkt_flag": order['off_mkt_flag'],
+                                      "mkt_type": order['mkt_type'],
+                                      "serial_no": order['serial_no'],
+                                      "group_id": order['group_id'],
+                                      })
+                conn.request("POST", "/orders/v1/cancel/regular",
+                             payload, headers)
+                res = conn.getresponse()
+                response_data = json.loads(res.read().decode("utf-8"))
+                if response_data.get("status"):
+                    # Return a success response
+                    return {"status": "success", "orderid": response_data['data'][0]['order_no']}, 200
+                else:
+                    # Return an error response
+                    return {"status": "error", "message": response_data.get("message", "Failed to cancel order")}, res.status
 
 
-def modify_order(data,auth):
-
-    
+def modify_order(data, auth):
 
     AUTH_TOKEN = auth
-    
-    newdata = transform_modify_order_data(data)  # You need to implement this function
-    
-  
+
+    # You need to implement this function
+    newdata = transform_modify_order_data(data)
+
     # Set up the request headers
     headers = {
         'X-Kite-Version': '3',
         'Authorization': f'token {AUTH_TOKEN}',
-        'Content-Type': 'application/x-www-form-urlencoded' 
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
     payload = {
         'order_type': newdata['order_type'],
@@ -185,11 +201,11 @@ def modify_order(data,auth):
         'trigger_price': newdata['trigger_price'],
         'disclosed_quantity': newdata['disclosed_quantity'],
         'validity': newdata['validity']
-      }
+    }
 
     print(payload)
 
-    payload =  urllib.parse.urlencode(payload)
+    payload = urllib.parse.urlencode(payload)
 
     conn = http.client.HTTPSConnection("api.kite.trade")
     conn.request("PUT", f"/orders/regular/{data['orderid']}", payload, headers)
@@ -201,32 +217,31 @@ def modify_order(data,auth):
         return {"status": "success", "orderid": data["data"]["order_id"]}, 200
     else:
         return {"status": "error", "message": data.get("message", "Failed to modify order")}, res.status
-    
 
-def cancel_all_orders_api(data,auth):
+
+def cancel_all_orders_api(data, auth):
 
     AUTH_TOKEN = auth
     # Get the order book
     order_book_response = get_order_book(AUTH_TOKEN)
-    #print(order_book_response)
+    # print(order_book_response)
     if order_book_response['status'] != 'success':
         return [], []  # Return empty lists indicating failure to retrieve the order book
 
     # Filter orders that are in 'open' or 'trigger_pending' state
     orders_to_cancel = [order for order in order_book_response.get('data', [])
-                        if order['status'] in ['OPEN', 'TRIGGER PENDING']]
+                        if order['status'] in ['Pending']]
     print(orders_to_cancel)
     canceled_orders = []
     failed_cancellations = []
 
     # Cancel the filtered orders
     for order in orders_to_cancel:
-        orderid = order['order_id']
-        cancel_response, status_code = cancel_order(orderid,AUTH_TOKEN)
+        orderid = order['order_no']
+        cancel_response, status_code = cancel_order(orderid, AUTH_TOKEN)
         if status_code == 200:
             canceled_orders.append(orderid)
         else:
             failed_cancellations.append(orderid)
-    
-    return canceled_orders, failed_cancellations
 
+    return canceled_orders, failed_cancellations
