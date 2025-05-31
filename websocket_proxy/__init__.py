d434f146219e07b22dcdef2e6ba3a5c55f457848
kalaivani <panlani@gmail.com>
2025-05-31 19:15:07 +0530

    Websockets Dynamic broker selection Update

--- websocket_proxy/__init__.py
+++ websocket_proxy/__init__.py
@@ -1,16 +1,23 @@
+# websocket_proxy/__init__.py
+
 from .server import WebSocketProxy, main as websocket_main
 from .broker_factory import register_adapter, create_broker_adapter
 
 # Import the angel_adapter directly from the broker directory
 from broker.angel.streaming.angel_adapter import AngelWebSocketAdapter
 
-# Register Angel adapter
+# Import the zerodha_adapter
+from broker.zerodha.streaming.zerodha_adapter import ZerodhaWebSocketAdapter
+
+# Register adapters
 register_adapter("angel", AngelWebSocketAdapter)
+register_adapter("zerodha", ZerodhaWebSocketAdapter)
 
 __all__ = [
     'WebSocketProxy',
     'websocket_main',
     'register_adapter',
     'create_broker_adapter',
-    'AngelWebSocketAdapter'
+    'AngelWebSocketAdapter',
+    'ZerodhaWebSocketAdapter'
 ]
