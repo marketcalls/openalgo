fdc2a8f15cace759d43b848fb92a4124a9503ad5
mohamedmuzamil008 <mohamedmuzamil@rocketmail.com>
2025-07-17 15:05:41 +0530

    Changed the openalgo API key

--- strategies/quote_example.py
+++ strategies/quote_example.py
@@ -7,16 +7,16 @@ import time
 
 # Initialize feed client with explicit parameters
 client = api(
-    api_key="e07835e0b2f2a765964ee4e6c2f741ada6f96ffa26211774026a368105482b4c",  # Replace with your API key
+    api_key="8009e08498f085ff1a3e7da718c5f4b585eaf9c2b7ce0c72740ab2b5d283d36c",  # Replace with your API key
     host="http://127.0.0.1:5000",  # Replace with your API host
     ws_url="ws://127.0.0.1:8765"  # Explicit WebSocket URL (can be different from REST API host)
 )
 
 # MCX instruments for testing
 instruments_list = [
-    {"exchange": "NSE_INDEX", "symbol": "NIFTY"},
-    {"exchange": "NSE", "symbol": "INFY"},
-    {"exchange": "NSE", "symbol": "TCS"}
+    #{"exchange": "NSE_INDEX", "symbol": "NIFTY"},
+    {"exchange": "NSE", "symbol": "RELIANCE"}
+    #{"exchange": "NSE", "symbol": "TCS"}
 ]
 
 def on_data_received(data):
@@ -28,10 +28,10 @@ client.connect()
 client.subscribe_quote(instruments_list, on_data_received=on_data_received)
 
 # Poll Quote data a few times
-for i in range(100):
-    print(f"\nPoll {i+1}:")
-    print(client.get_quotes())
-    time.sleep(0.5)
+for i in range(3600):
+    #print(f"\nPoll {i+1}:")
+    #print(client.get_quotes())
+    time.sleep(1)
 
 # Cleanup
 client.unsubscribe_quote(instruments_list)
