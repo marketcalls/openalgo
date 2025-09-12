8f02b4a220f8e9abf7eeaaefa09cda29b8a78d37
rajandran <rajandran@marketcalls.in>
2025-09-12 09:13:09 +0530

    Fix parameter name in get_oa_symbol function call in map_order_data to Fix Zerodha Orderbook and TradeBook

--- broker/zerodha/mapping/order_data.py
+++ broker/zerodha/mapping/order_data.py
@@ -36,7 +36,7 @@ def map_order_data(order_data):
             
             # Check if a symbol was found; if so, update the trading_symbol in the current order
             if symbol:
-                order['tradingsymbol'] = get_oa_symbol(symbol=symbol,exchange=exchange)
+                order['tradingsymbol'] = get_oa_symbol(brsymbol=symbol,exchange=exchange)
             else:
                 logger.info(f"{symbol} and exchange {exchange} not found. Keeping original trading symbol.")
                 
