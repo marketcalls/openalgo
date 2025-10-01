2fe5655200ffd781034ae027aef74fe4bfb6d0d8
kalaivani <panlani@gmail.com>
2025-10-01 12:13:20 +0530

    Openalgo Sandbox Initialization Update

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ from database.chartink_db import init_db as ensure_chartink_tables_exists
 from database.traffic_db import init_logs_db as ensure_traffic_logs_exists
 from database.latency_db import init_latency_db as ensure_latency_tables_exists
 from database.strategy_db import init_db as ensure_strategy_tables_exists
+from database.sandbox_db import init_db as ensure_sandbox_tables_exists
 
 from utils.plugin_loader import load_broker_auth_functions
 
@@ -308,6 +309,7 @@ def setup_environment(app):
         ensure_traffic_logs_exists()
         ensure_latency_tables_exists()
         ensure_strategy_tables_exists()
+        ensure_sandbox_tables_exists()
 
     # Conditionally setup ngrok in development environment
     if os.getenv('NGROK_ALLOW') == 'TRUE':
