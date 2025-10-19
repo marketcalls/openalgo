b04ac0afbc57b1773cb28b6cf0643d7b83d87be2
rajandran <rajandran@marketcalls.in>
2025-10-19 14:05:48 +0530

    Fix : turn off http2 in docker sciprt for ubuntu

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -407,9 +407,8 @@ server {
 
 # HTTPS - Main Configuration
 server {
-    listen 443 ssl;
-    listen [::]:443 ssl;
-    http2 on;
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
     
     server_name $DOMAIN;
     
