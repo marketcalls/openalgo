7074831cf690a8c04b88f5c7526b40076ea8538d
rajandran <rajandran@marketcalls.in>
2025-11-25 13:48:14 +0530

    updated openalgo version to 1.0.0.39 and updated python sdk to 1.0.40 added verbose control in the websockets

--- utils/version.py
+++ utils/version.py
@@ -1,7 +1,7 @@
 # OpenAlgo Version Management
 # This file is the single source of truth for version information
 
-VERSION = '1.0.0.38'
+VERSION = '1.0.0.39'
 
 def get_version():
     """Return the current OpenAlgo version"""
