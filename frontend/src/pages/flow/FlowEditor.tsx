9e67cefac551f31991fa5ad6932f5e5f9226f1a5
rajandran <rajandran@marketcalls.in>
2026-08-23 11:22:50 +0530

    fix(flow): cap initial editor zoom

--- frontend/src/pages/flow/FlowEditor.tsx
+++ frontend/src/pages/flow/FlowEditor.tsx
@@ -921,6 +921,7 @@ function FlowEditorContent() {
             nodeTypes={nodeTypes}
             edgeTypes={edgeTypes}
             fitView
+            fitViewOptions={{ maxZoom: 0.6 }}
             snapToGrid
             snapGrid={[16, 16]}
             deleteKeyCode={null}
